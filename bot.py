54082d35a7da7707fb6707af5290051abb7c2114
Tobiasz Kostrzewa <235900@edu.p.lodz.pl>
2022-05-17 20:44:47 +0200

    Create Bot

---
+++ bot.py
@@ -0,0 +1,38 @@
+import datetime
+import time
+import socketio
+from tgtg import TgtgClient
+import sys
+import json
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()  # take environment variables from .env.
+
+client = TgtgClient(access_token=os.environ.get("ACCESS_TOKEN"), refresh_token=os.environ.get("REFRESH_TOKEN"), user_id=os.environ.get("USER_ID"))
+
+sio = socketio.Client()
+sio.connect('http://localhost:8484')
+
+enabled = True
+
+while True:
+    now = datetime.datetime.now()
+    if(now.hour > 16 and now.hour < 22 and enabled):
+        try:
+            print("Waiting for server")
+            response = client.get_items()
+        except Exception as e:
+            print('Error getting items: {}'.format(e))
+        sio.emit('python-message', response)
+        #print("response sent")
+        str = '{}:{} response sent'.format(now.hour, now.minute)
+        print(str)
+        sleep_time = 60 * 1 #check every 1 min
+    else:
+        #print("Not the time yet! Sleeping...")
+        str = '{}:{} is not the time yet! Sleeping...'.format(now.hour, now.minute)
+        print(str)
+        sleep_time = 60 * 30 #30 min
+    time.sleep(sleep_time)
---
+++ index.js
@@ -0,0 +1,138 @@
+var express = require("express");
+var app = express();
+var http = require("http");
+var https = require("https");
+app.use(express.static("./public"));
+const nodemailer = require("nodemailer");
+var http_server = http.createServer(app).listen(8484);
+var http_io = require("socket.io")(http_server);
+const dotenv = require('dotenv');
+dotenv.config();
+
+enabled = 1
+gamealive = true;
+
+
+const { Client, Intents, Modal, Channel, MessageEmbed } = require('discord.js');
+
+/*const exampleEmbed = new MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });*/
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+
+// When the client is ready, run this code (only once)
+client.once('ready', async () => {
+  //const channel = await client.channels.fetch('796126423453663315');
+	console.log('Ready!');
+  //channel.send("@everyone Hello world");
+});
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
+
+
+const options = {
+  hostname: 'thedrinkitgame.pl',
+  port: 443,
+  path: '/',
+  method: 'GET',
+};
+
+setInterval(async () => {
+  const channel = await client.channels.fetch('976106571924897843');
+
+  const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+    if(res.statusCode != 200 && gamealive){
+      const exampleEmbed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Gra umarła')
+      .setURL('https://thedrinkitgame.pl/')
+      //.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+      .setDescription('Zrób coś kurła')
+      .setThumbnail('https://cdn.discordapp.com/attachments/796126423453663315/960122476040429568/DrinkIt_new.png')
+      //.addFields(
+      //  { name: 'Regular field title', value: 'Some value here' },
+      //  { name: '\u200B', value: '\u200B' },
+      //  { name: 'Inline field title', value: 'Some value here', inline: true },
+      //  { name: 'Inline field title', value: 'Some value here', inline: true },
+      //)
+      //.addField('Inline field title', 'Some value here', true)
+      //.setImage('https://i.imgur.com/AfFp7pu.png')
+      .setTimestamp()
+      .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+      gamealive = false;
+      channel.send({ embeds: [exampleEmbed] });
+    }
+    else if(res.statusCode == 200 && !gamealive){
+      const exampleEmbed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Gra ożyła')
+      .setURL('https://thedrinkitgame.pl/')
+      .setThumbnail('https://cdn.discordapp.com/attachments/796126423453663315/960122476040429568/DrinkIt_new.png')
+      .setDescription('Gratuluję')
+      .addFields(
+        { name: 'Response status', value: res.statusCode })
+      .setTimestamp()
+      gamealive = true;
+      channel.send({ embeds: [exampleEmbed] });
+    }
+    
+  });
+  
+  req.on('error', error => {
+    console.error(error);
+  });
+  
+  req.end();
+},60 * 1000)
+
+http_io.on("connection", async function(httpsocket){
+  const channel = await client.channels.fetch('976106571924897843');
+  console.log("new user");
+    httpsocket.on('python-message', function(fromPython){
+        httpsocket.broadcast.emit('message', fromPython);
+        //console.log(fromPython);
+        var count = Object.keys(fromPython).length;
+        //console.log(count);
+        for(i = 0; i < count; i++){
+          item = fromPython[i];
+          if(item["items_available"] != 0 && enabled){
+            console.log(item["display_name"] + " available!");
+            const exampleEmbed = new MessageEmbed()
+              //.setColor('#0099ff')
+              .setTitle(item["display_name"])
+              //.setURL('https://thedrinkitgame.pl/')
+              .setDescription('Szybko bo ktoś nam zaraz znowu zapierdoli')
+              .setTimestamp()
+              .addFields({ name: 'Dostępna ilość', value: item["items_available"] })
+
+            if(item["display_name"] === "Starbucks Łódź Piotrkowska (Na koniec dnia)")
+              exampleEmbed.setColor("#32a852")
+            else
+              exampleEmbed.setColor("#96651a")
+            
+            channel.send({ embeds: [exampleEmbed] });
+            //channel.send(item["display_name"] + " dostępny w TGTG!");
+            //send_notification(item);
+        }
+      }
+    })
+})
