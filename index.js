080213f45f4e3f88aaff7ff1c3ebcbfcc1b9175d
Tobiasz-PC <tobiasz.kostrzewa@wp.pl>
2022-06-27 17:22:36 +0200

    No idea

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ setInterval(async () => {
       //.addField('Inline field title', 'Some value here', true)
       //.setImage('https://i.imgur.com/AfFp7pu.png')
       .setTimestamp()
-      .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+      //.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
       gamealive = false;
       channel.send({ embeds: [exampleEmbed] });
     }
